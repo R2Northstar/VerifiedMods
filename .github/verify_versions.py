ca64906251273e022a39ebcc2d7ee58ebf43e84e
Remy Raes <raes.remy@gmail.com>
2024-01-25 21:06:29 +0100

    docs: add documentation to both methods

--- .github/verify_versions.py
+++ .github/verify_versions.py
@@ -3,6 +3,14 @@ from urllib.request import urlopen
 import json
 
 def verify_all_mod_versions():
+    """
+    Ensure all mod versions are properly declared to GitHub.
+
+    For each mod entry of this repository's manifesto, this method will fetch information
+    from GitHub API and compare declared SHA ("CommitHash" member) to GitHub information,
+    to ensure tracability of verified mods.
+    """
+
     # Load local manifesto file
     f = open('verified-mods.json')
     manifesto = json.load(f)
@@ -25,7 +33,20 @@ def verify_all_mod_versions():
             else:
                 sys.exit('  ❌ v{} (hash comparison failed)'.format(version['Version']))
 
+
 def retrieve_tag_info(tag_name, repository_url):
+    """
+    Retrieves tag information from distant API.
+
+    Since the GitHub API is paginated, (*i.e.* it does not list all data in a single page,
+    but rather serves pages holding 30 elements maximum), we need to browse all pages
+    until either the tag is found or the page is empty, meaning we didn't find the tag.
+    Page browsing is done by updating the `page` URL argument (`?page=1`, `?page=2` etc).
+
+    @param tag_name: the name of the mod release
+    @param repository_url: API URL used to access mod's tags data
+    @return: tag data, including commit SHA signature
+    """
     i = 1
     while True:
         url = '{}?page={}'.format(repository_url, i)
@@ -41,6 +62,7 @@ def retrieve_tag_info(tag_name, repository_url):
         if len(matching_distant_versions) == 1:
             return matching_distant_versions[0]
         i += 1
+
 
 if __name__ == "__main__":
     verify_all_mod_versions()
