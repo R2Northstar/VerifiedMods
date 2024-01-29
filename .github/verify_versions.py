915d5b945388cc6fa6115b047fd2dfd1fe053c10
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2024-01-29 16:05:47 +0100

    refactor: Put current verification code into function (#30)
    Refactoring to allow for easier adding support for API pagination
    
    Co-authored-by: Remy Raes <raes.remy@gmail.com>

--- .github/verify_versions.py
+++ .github/verify_versions.py
@@ -2,31 +2,35 @@ import sys
 from urllib.request import urlopen
 import json
 
-# Load local manifesto file
-f = open('verified-mods.json')
-manifesto = json.load(f)
+def verify_all_mod_versions():
+    # Load local manifesto file
+    f = open('verified-mods.json')
+    manifesto = json.load(f)
 
-for mod in manifesto:
-    print('Verifying "{}":'.format(mod))
+    for mod in manifesto:
+        print('Verifying "{}":'.format(mod))
 
-    # Build GitHub API link and fetch distant tags list
-    words = manifesto[mod]['Repository'].split('/')
-    tags_url = "https://api.github.com/repos/{}/{}/tags".format(words[-2], words[-1])
-    response = urlopen(tags_url) 
-    tags_data = json.loads(response.read())
+        # Build GitHub API link and fetch distant tags list
+        words = manifesto[mod]['Repository'].split('/')
+        tags_url = "https://api.github.com/repos/{}/{}/tags".format(words[-2], words[-1])
+        response = urlopen(tags_url) 
+        tags_data = json.loads(response.read())
 
-    # Check all mod versions one-by-one
-    for version in manifesto[mod]['Versions']:
-        local_hash = version['CommitHash']
-        matching_distant_versions = list(filter(lambda v: v['name'] == version['Version'], tags_data))
+        # Check all mod versions one-by-one
+        for version in manifesto[mod]['Versions']:
+            local_hash = version['CommitHash']
+            matching_distant_versions = list(filter(lambda v: v['name'] == version['Version'], tags_data))
 
-        # There should be only one matching version
-        if len(matching_distant_versions) != 1:
-            sys.exit('  ❌ v{} (unknown distant version)'.format(version['Version']))
-        
-        # Compare manifesto commit hash with repository hash
-        distant_version = matching_distant_versions[0]
-        if local_hash == distant_version['commit']['sha']:
-            print('  ✔️ v{}'.format(version['Version']))
-        else:
-            sys.exit('  ❌ v{} (hash comparison failed)'.format(version['Version']))
+            # There should be only one matching version
+            if len(matching_distant_versions) != 1:
+                sys.exit('  ❌ v{} (unknown distant version)'.format(version['Version']))
+            
+            # Compare manifesto commit hash with repository hash
+            distant_version = matching_distant_versions[0]
+            if local_hash == distant_version['commit']['sha']:
+                print('  ✔️ v{}'.format(version['Version']))
+            else:
+                sys.exit('  ❌ v{} (hash comparison failed)'.format(version['Version']))
+
+if __name__ == "__main__":
+    verify_all_mod_versions()
